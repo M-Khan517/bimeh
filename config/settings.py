8acdb97ebd0480bb4bab4d1589ce0f6c5f3d3c62
M-Khan517 <113201585+M-Khan517@users.noreply.github.com>
2025-07-14 19:10:45 +0330

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -99,10 +99,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
+    
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'NAME': 'mydb',
@@ -111,6 +112,12 @@ DATABASES = {
     #     'HOST':'localhost',
     #     'PORT':'3306',
     # }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": BASE_DIR / "db.postgresql",
+        "USER":"root",
+        "PASSWORD":"32769272",
+    }
 }
 
 AUTH_USER_MODEL = "account_module.User"
